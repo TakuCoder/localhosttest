6b0f4796ac6c095461a01390ae7d7433a1acfcf8
thiyagu <thiyagu@programmer.net>
2022-12-21 18:44:35 +0530

    initia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
   title = 'urlTest';
+
+  constructor(private httpClient : HttpClient){
+
+  }
+
+
+  fireLocal() {
+    this.httpClient.get<any>('http://localhost:7553/api/home/ping').subscribe(data => {
+        console.log(data);
+    })   
+  }
 }
